c08aacdc6387a47bd496df235bde2acd3b4ff03f
Михаил Харлов <p4kc0dn43eml1@gmail.com>
2022-02-15 15:21:38 +0300

    autocomplete component

--- autocomplete.jsx
+++ autocomplete.jsx
@@ -1,15 +1,61 @@
 const data = require("./data.json");
+import { useEffect, useState } from "react";
 
-const Autocomplete = (props) => {
+const Autocomplete = () => {
+  const [input, setInput] = useState("");
+  const [filteredData, setFilteredData] = useState();
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(false);
+
+  const changeHandler = (e) => {
+    setSelected(false);
+    setInput(e.target.value);
+    setVisible(true);
+  };
+
+  useEffect(() => {
+    if (input && !selected) {
+      setFilteredData(
+        data.filter((item) =>
+          item.name.toLowerCase().includes(input.toLocaleLowerCase())
+        )
+      );
+    } else {
+      setFilteredData(data);
+    }
+  }, [input, selected]);
+
+  const toggleHandler = (e) => {
+    setVisible((prevState) => !prevState);
+  };
+
+  const onClickHandler = (val) => {
+    setInput(val);
+    setVisible(false);
+    setSelected(true);
+  };
+
   return (
-    <select>
-      <option value="none" selected disabled hidden>
-        Select name
-      </option>
-      {data.map((item) => (
-        <option value={item.name}>{item.name}</option>
-      ))}
-    </select>
+    <div>
+      <input
+        onChange={changeHandler}
+        value={input || ""}
+        onClick={toggleHandler}
+      />
+
+      <ul className="suggestions">
+        {visible &&
+          filteredData &&
+          filteredData.map((item, i) => (
+            <li key={i} onClick={() => onClickHandler(item.name)}>
+              {item.name}
+            </li>
+          ))}
+        {visible && filteredData && filteredData.length === 0 && (
+          <li className="no-options">no options</li>
+        )}
+      </ul>
+    </div>
   );
 };
 
